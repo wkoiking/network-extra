e67036a4dffd4242cff8a3c0d1786b8eecc888e8
Nao Fujinaga <wanagida@gmail.com>
2020-10-29 12:02:57 +0900

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,95 @@
+﻿module Main where
+
+import GHC.Generics (Generic)
+import Network.Socket.ByteString.Extra
+import Control.Concurrent
+import Network.Socket
+import Data.Fixed (Milli)
+import Control.Exception
+import System.IO
+import Data.List (intercalate)
+-- cereal
+import Data.Serialize (Get, Serialize(..))
+-- time
+import Data.Time.Format
+import Data.Time.LocalTime
+import Data.Time.Clock
+
+showIndianTime :: UTCTime -> String
+showIndianTime = formatTime timeLocale "%d/%m/%Y %k:%M:%S" . utcToLocalTime timeZone
+
+timeLocale :: TimeLocale
+timeLocale = defaultTimeLocale { knownTimeZones = [indiaTimeZone] }
+
+timeZone :: TimeZone
+timeZone = indiaTimeZone
+
+indiaTimeZone :: TimeZone
+indiaTimeZone = TimeZone
+    { timeZoneMinutes = 60 * 5 + 30
+    , timeZoneSummerOnly = False
+    , timeZoneName = "IST"
+    }
+
+data MyData
+    = MyData1
+    | MyData2
+    deriving (Show, Generic, Serialize)
+
+main :: IO ()
+main = do
+    vStdout <- newMVar stdout :: IO (MVar Handle)
+    let logger :: String -> String -> IO ()
+        logger label str = do
+            time <- getCurrentTime
+            let timeStr :: String
+                timeStr = showIndianTime time ++ " "
+                tagStr = label ++ ": "
+                spaces = replicate (length timeStr) ' '
+                headerStr = concat [timeStr, tagStr]
+                tailStr = concat [spaces, tagStr]
+            withMVar vStdout $ \ h -> hPutStrLn h $ concat [intercalate "\n" $ zipWith (++) (headerStr : repeat tailStr) $ lines str]
+    let errorHandler :: SomeException -> IO ()
+        errorHandler err = logger "" $ show err
+
+    -- Server
+    _ <- forkIO $ runTcpServer (logger "Server1") port (5000 * 1000) $ procRecvVal (get :: Get MyData) $ \ a -> do
+        logger "Server1" $ show a
+        return ()
+
+--     forkIO $ bracket (openSockTCPServer port) close $ \ mastersock -> forever $ do
+--         (connsock, clientaddr) <- accept mastersock
+--         log $ "Connected: " ++ show clientaddr
+--         forkIO $ handle errorHandler $ (`finally` waitFinAndClose connsock) $ do
+--             _ <- recv connsock 4096
+--             return ()
+
+    -- Client1
+    _ <- forkIO $ handle errorHandler $ withSocket "localhost" port $ \ sock addr -> do
+        setSocketOption sock KeepAlive 1
+        connect sock addr
+        logger "Client1" "connect"
+        wait 10
+        sendValue put sock $ (-1 :: Int)
+        logger "Client1" "sent"
+        logger "Client1" "close"
+
+    -- Client2
+    _ <- forkIO $ handle errorHandler $ withSocket "localhost" port $ \ sock addr -> do
+        setSocketOption sock KeepAlive 1
+        connect sock addr
+        logger "Client2" "connect"
+        wait 2
+        sendValue put sock $ (-1 :: Int)
+        logger "Client2" "sent"
+        logger "Client2" "close"
+
+    wait 20
+
+wait
+    :: Milli -- ^ seconds
+    -> IO ()
+wait s = threadDelay $ truncate $ s * 1000000
+
+port :: String
+port = "60103"
---
+++ src/Network/Socket/ByteString/Extra.hs
@@ -0,0 +1,224 @@
+module Network.Socket.ByteString.Extra
+    ( runTcpServer
+    , procRecvVal
+    , sendValueCompressed
+    , recvValueCompressed
+    , sendValue
+    , recvValue
+    , recvAll
+    , openSockUDPSender
+    , openSockUDPReceiver
+    , openSockTCPServer
+    , openSockTCPClient
+    , waitFinAndClose
+    , withSocket
+    )
+where
+
+import GHC.Generics (Generic)
+-- network-socket-options
+import Network.Socket.Options
+-- network
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString (recv, sendMany)
+-- bytestring
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as LB hiding (unpack)
+import qualified Data.ByteString.Lazy.Char8 as LB (unpack)
+import Data.ByteString.Builder (byteStringHex, toLazyByteString)
+
+-- safe-exceptions
+import Control.Exception.Safe
+-- base
+import Control.Monad (when, forever)
+import Control.Concurrent (forkIO)
+import Data.List (splitAt, intercalate)
+-- cereal
+import Data.Serialize (runGetPartial, runPutLazy, decodeLazy, encodeLazy, Get, Putter, Result(..), Serialize(..))
+-- deepseq
+import Control.DeepSeq
+-- zlib
+import Codec.Compression.GZip (compress, decompress)
+
+newtype Compressed = Compressed LB.ByteString
+    deriving stock (Eq, Show, Generic)
+    deriving newtype (NFData, Serialize)
+
+sendValueCompressed
+    :: Serialize a
+    => Socket
+    -> a
+    -> IO ()
+sendValueCompressed sock val = sendValue put sock $ Compressed $ compress $ encodeLazy val
+
+recvValueCompressed
+    :: Serialize a
+    => Socket
+    -> Int -- ^ maximum number of byte for each chunk to receive
+    -> IO (Either String a)
+recvValueCompressed sock n = do
+    (eVal, _) <- recvValue get sock n
+    return $ do
+        Compressed bstr <- eVal
+        decodeLazy $ decompress bstr
+
+
+-- | receive stream until the message is successfully parsed or failed for TCP/IP
+
+sendValue
+    :: Putter a
+    -> Socket
+    -> a
+    -> IO ()
+sendValue putter sock val = sendMany sock $ LB.toChunks $ runPutLazy $ putter val
+
+recvValue
+    :: Get a
+    -> Socket
+    -> Int -- ^ maximum number of byte for each chunk to receive
+    -> IO (Either String a, ByteString)
+recvValue getter sock n = receiveWithCont $ runGetPartial getter
+ where receiveWithCont :: (ByteString -> Result a) -> IO (Either String a, ByteString)
+       receiveWithCont cont = do
+           first <- recv sock n
+           case cont first of
+               Done a _ -> return $ (Right a, first)
+               Fail err _ -> return $ (Left err, first)
+               Partial cont' -> do
+                   (result, rest) <- receiveWithCont cont'
+                   return (result, first <> rest)
+
+-- | receive all stream until FIN received for TCP/IP
+recvAll
+    :: Socket
+    -> Int -- ^ maximum number of byte for each chunk to receive
+    -> IO ByteString
+recvAll sock n = do
+    first <- recv sock n
+    if B.length first == 0
+        then return mempty
+        else do
+            rest <- recvAll sock n
+            return $ first <> rest
+
+openSockUDPSender
+     :: Bool -- ^ isBroadCast
+     -> String -- ^ IP Address
+     -> String -- ^ Port Number
+     -> IO (Socket, SockAddr)
+openSockUDPSender isBroadCast addr port = do
+     addrinfos <- getAddrInfo Nothing (Just addr) (Just port)
+     let receiverAddr = head addrinfos
+     sock <- socket AF_INET Datagram 0
+     when isBroadCast $ setSocketOption sock Broadcast 1
+     setSocketOption sock SendBuffer 1472
+     return (sock, addrAddress receiverAddr)
+
+openSockUDPReceiver
+    :: String -- ^ Port number or name
+    -> IO Socket
+openSockUDPReceiver port = do
+    addrinfos <- getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]})) Nothing (Just port)
+    let serveraddr = head addrinfos
+    sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+    bind sock (addrAddress serveraddr)
+    return sock
+
+openSockTCPServer
+    :: String -- ^ Port number or name
+    -> IO Socket
+openSockTCPServer port = do
+    addrinfos <- getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]})) Nothing (Just port)
+    let serveraddr = head addrinfos
+    sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+    bind sock (addrAddress serveraddr)
+    listen sock 2048
+    return sock
+
+openSockTCPClient
+    :: HostName -- ^ IP Address of the target server to communicate
+    -> ServiceName -- ^ Port number of the target server to communicate
+    -> IO Socket
+openSockTCPClient hostname port = do
+    addrinfos <- getAddrInfo Nothing (Just hostname) (Just port)
+    let serveraddr = head addrinfos
+    sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+--     setSocketOption sock KeepAlive 1
+    connect sock (addrAddress serveraddr)
+    return sock
+
+waitFinAndClose :: Socket -> IO ()
+waitFinAndClose sock = do
+--     { shutdown sock ShutdownSend
+    { _ <- recvAll sock 1024
+    ; return ()
+    } `finally` close sock
+
+withSocket
+    :: HostName -- ^ IP Address of the target server to communicate
+    -> ServiceName -- ^ Port number of the target server to communicate
+    -> (Socket -> SockAddr -> IO a)
+    -> IO a
+withSocket hostname port io = bracket initializer (close . fst) (uncurry io)
+ where initializer = do
+           addrinfos <- getAddrInfo Nothing (Just hostname) (Just port)
+           let serveraddr = head addrinfos
+           sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+           return (sock, addrAddress serveraddr)
+
+runTcpServer
+    :: (String -> IO ())
+    -> ServiceName
+    -> Microseconds -- ^ timeout of receiving each chunk in microseconds
+    -> (Socket -> SockAddr -> IO ()) -> IO ()
+runTcpServer logger port timeoutMicroSec proc = bracket (openSockTCPServer port) close $ \ mastersock -> forever $ do
+    (connsock, clientaddr) <- accept mastersock
+    setRecvTimeout connsock timeoutMicroSec
+
+    let log' :: String -> IO ()
+        log' str = logger $ intercalate "\n" $ map unwords
+            [ ["Exeption for", port, "<==", show clientaddr]
+            , [str]
+            ]
+
+    let errorHandler :: SomeException -> IO ()
+        errorHandler err = case fromException err :: Maybe StringException of
+            Just (StringException str _) -> log' str
+            Nothing                      -> log' $ unwords ["error message:", show err]
+    forkIO $ handle errorHandler $ (`finally` waitFinAndClose connsock) $ proc connsock clientaddr
+
+procRecvVal :: Get a -> (a -> IO ()) -> Socket -> SockAddr -> IO ()
+procRecvVal getter proc sock _  = do
+    (eVal, msg) <- recvValue getter sock 4096 -- 2 ^ 13
+    case eVal of
+        Right val -> proc val
+        Left err -> throwString $ unlines $ map unwords
+            [ ["packet length:", show $ B.length msg]
+            , ["decode error message:", err]
+            , ["contents:"]
+            , [showByteStringHex msg]
+            ]
+
+showByteStringHex :: ByteString -> String
+showByteStringHex = formatHex . LB.unpack . toLazyByteString . byteStringHex
+
+formatHex :: String -> String
+formatHex xs = unlines
+    [ replicate 10 ' ' ++ unwords (map (padTo'' 2 . show) ([0 .. 9] :: [Int]))
+    , unlines $ map (\ (i, str) -> concat [take 10 $ show i ++ repeat ' ', str]) $ zip ([0..] :: [Int]) $ map unwords x00_00s
+    ]
+ where x00s = toTable 2 xs
+       x00_00s = toTable 10 x00s
+
+padTo'' :: Int -> String -> String
+padTo'' i str = reverse $ take i $ reverse str ++ repeat ' '
+
+toTable
+    :: Int
+    -> [a]
+    -> [[a]]
+toTable _ [] = []
+toTable n xs = case splitAt n xs of
+    (ls, []) -> [ls]
+    (ls, rs) -> ls : toTable n rs
